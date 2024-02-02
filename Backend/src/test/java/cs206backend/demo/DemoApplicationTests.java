2694eb8cef7e72ce83a7a99b405dcb1c830963d2
Defyious <jevontanjinghong@gmail.com>
2024-02-03 00:23:15 +0800

    SpringBoot-Initialisation

---
+++ Backend/src/test/java/cs206backend/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cs206backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
